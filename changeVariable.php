c0f8c6d204d2ab35f0e1b17615e41ead129b81d8
Tatsuro Muto <tomuto0221@gmail.com>
2021-01-30 10:40:15 +0900

    changeVariable.phpファイル追加

---
+++ changeVariable.php
@@ -0,0 +1,32 @@
+<?php
+    //名前用の変数
+    $name = "神田ごろう";
+
+    //身長用の変数
+    $height = 169.5;
+
+    //年齢用の変数
+    $age = 19;
+ ?>
+ <html>
+     <head>
+         <meta http-equiv="Content-Type"content="text/html;charset=UTF-8">
+     </head>
+     <body>
+    <?php
+        echo$name,'さんの身長は',$height,"cmです。<br>";
+        echo"年齢は",$age,"歳です。";
+
+        //$ageを20に変更
+        $age=20;
+
+        echo"あと1ヶ月で",$age,"歳になります。";
+     ?>
+
+     </body>
+ </html>
+
+
+
+
+
