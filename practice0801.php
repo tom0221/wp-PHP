d6278b5801744f46b51cbe8e67b87532fbd68cd4
Tatsuro Muto <tomuto0221@gmail.com>
2021-02-05 19:14:47 +0900

    practice0801.php作成

---
+++ practice0801.php
@@ -0,0 +1,19 @@
+<?php
+	$num1 = 44;
+	$num2 = 5;
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type"content="text/html; charset=UTF-08">
+	</head>
+	<body>
+	<?php
+		echo $num1, ' + ', $num2, ' = ', $num1 + $num2, '<br>';
+		echo $num1, ' - ', $num2, ' = ', $num1 - $num2, '<br>';
+		echo $num1, ' * ', $num2, ' = ', $num1 * $num2, '<br>';
+		echo $num1, ' % ', $num2, ' = ', $num1 / $num2, '<br>';
+	?>
+	</body>
+</html>
+
+
