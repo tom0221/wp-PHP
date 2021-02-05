65d8654a13ffe4a1b098a460bcc2c52f8b275ba8
Tatsuro Muto <tomuto0221@gmail.com>
2021-02-05 19:02:06 +0900

    useArrayCalc.php作成

---
+++ useArrayCalc.php
@@ -0,0 +1,19 @@
+<?php
+	//配列変数に3つの値を用意
+	$nums = array(1,3,5,);
+
+	//配列の要素を3つ全て足す
+	$ans = $nums[0] + $nums[1] + $nums[2];
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type"content="text/html; charset=UTF-08">
+	</head>
+	<body>
+	<?php
+		echo $nums[0], '+', $nums[1], '+', $nums[2], '=', $ans;
+	?>
+	</body>
+</html>
+
+
