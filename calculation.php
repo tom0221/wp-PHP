19c8fa233417acece50a255c8f589e3f53659135
Tatsuro Muto <tomuto0221@gmail.com>
2021-02-05 10:48:07 +0900

    calculation.php作成

---
+++ calculation.php
@@ -0,0 +1,23 @@
+<?php
+	$data1 = 10+8;
+	$data2 = 10-8;
+	$data3 = 10*8;
+	$data4 = 10/8;
+	$data5 = 10%8;
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type"content="text/html; charset=UTF-08">
+	</head>
+	<body>
+	<?php
+		echo"10+8 =", $data1,"<br>";
+		echo"10-8 =", $data2,"<br>";
+		echo"10*8 =", $data3,"<br>";
+		echo"10/8 =", $data4,"<br>";
+		echo"10%8 =", $data5,"<br>";
+	?>
+	</body>
+</html>
+
+
